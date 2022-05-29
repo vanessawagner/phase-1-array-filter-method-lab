20ff94d4453591c372d0b3ce7559e35cdaf4f3bd
Vanessa Wagner <vmwagner@gmail.com>
2022-05-29 09:25:18 -0500

    commit tree 2eaa87a8bb30243d1cfadb9db3796e3b62f2a268

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching (driver, argument) {
+    return driver.filter(function(name) {
+        return name.toLowerCase() === argument.toLowerCase();
+    } )
+}
+
+// function fuzzyMatch(driver, argument) {
+//     return driver.filter(function(name) {
+//         return name.toLowerCase().indexOf(argument.toLowerCase()) !== -1;
+//     })
+// }
+
+function fuzzyMatch (driver, argument) {
+    return driver.filter(function(name) {
+        //return name.toLowerCase().indexOf(argument.toLowerCase())!==-1
+        return name.startsWith(argument)
+    })
+}
+
+function matchName(driver, argument) {
+    const result = driver.filter(driver => driver.name === argument);
+    return result
+}
